f37ed1ff79fbc550ee198bc6b9eb97d784828b20
Mateus Roveda <mateus@roveeb.com>
2026-08-14 22:18:27 -0300

    add correcao exercicio

---
+++ 3-aula1408-correcao-exercicio/App.js
@@ -0,0 +1,9 @@
+import { View, StyleSheet } from "react-native";
+
+export default function App() {
+  return;
+  <View style={{ flex: 1, paddingTop: 60, paddingLeft: 30 }}>
+    <t</View>;
+}
+
+const estilos = StyleSheet.create({});
