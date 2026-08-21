923bec2d3830ae73d45ba3d98ab78db12ae488cf
Mateus Roveda <mateus@roveeb.com>
2026-08-21 13:47:31 -0300

    add aula3

--- 3-aula1408-correcao-exercicio/App.js
+++ 3-aula1408-correcao-exercicio/App.js
@@ -1,9 +1,39 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 export default function App() {
-  return;
-  <View style={{ flex: 1, paddingTop: 60, paddingLeft: 30 }}>
-    <t</View>;
+  return (
+    <View style={styles.viewPrincipal}>
+      <View style={styles.cabecalho}>
+        <View style={styles.circulo}></View>
+        <View style={styles.viewTextos}>
+          <Text style={{ fontSize: 30 }}>Olá, estudante!</Text>
+          <Text>Boas vindas.</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
 
-const estilos = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewPrincipal: {
+    flex: 1,
+    paddingTop: 60,
+  },
+
+  circulo: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    backgroundColor: "gray",
+  },
+
+  cabecalho: {
+    gap: 20,
+    flexDirection: "row",
+    marginLeft: 10,
+  },
+
+  viewTextos: {
+    justifyContent: "center",
+  },
+});
--- 3-aula1408/App.js
+++
@@ -1,9 +0,0 @@
-import { View, StyleSheet } from "react-native";
-
-export default function App() {
-  return;
-  <View style={{ flex: 1, paddingTop: 60, paddingLeft: 30 }}>
-    <t</View>;
-}
-
-const estilos = StyleSheet.create({});
